8f5ea83ee1285e54a55ee594f4dd75e231d009e9
Ibukov Dionis <dionis.ibukov@gmail.com>
2023-12-08 23:01:55 +0500

    zadanie 1 - yes!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Seminar04 {
+    static void Zadanie01(){
+        Console.WriteLine("Программа бесконечно запрашивает числа с консолию. \nПрограмма завершается при вводе символа 'q' или при вводе числа, \nсумма цифр которого чётная.");
+        
+        Console.Write("Введите число: ");
+        string a = Console.ReadLine();
+        int n = 0;
+        
+        while (a != "q") {
+            
+            
+            if (int.TryParse(a,out n)){
+                n = int.Parse(a);
+            }
+            int summ = 0;
+            for (int i=0; i <= a.Length; i++){
+                summ += n%10;
+                n = n / 10;
+            }
+            
+            if (summ%2 == 0) {
+                Console.WriteLine("Сумма цифр четно!");
+                break;
+            }
+            
+            Console.Write("Введите число: ");
+            a = Console.ReadLine();
+            
+        }
+        Console.WriteLine("Завершение прграммы!");
+                
+    
+
+    }
+    static void Main(){
+        Console.Clear();
+        Zadanie01();
+        Zadanie02();
+    }
+}
